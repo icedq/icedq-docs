224b43c3e335ac7b2d92e8e4a9d2380626609ce4
Dnyaneshwar Muley <dnyaneshwar.m@toranainc.com>
2023-03-31 19:10:53 +0530

    url update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/icedq-log-wo-com-favicon.png',
 
   // Set the production url of your site here
-  url: 'https://docs.icedq.com',
+  url: 'https://ngdocs.icedq.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
